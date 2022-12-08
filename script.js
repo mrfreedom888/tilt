9f61f1cf540c2241ee28ae2ab4a1465a90946fdf
mrfreedom888 <example@gmail.com>
2022-12-08 14:02:53 +0200

    frist

---
+++ script.js
@@ -0,0 +1,73 @@
+// 'use strict';
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;x
+// const leftBorderWidth = 1;
+
+// number = 20;
+// console.log(number);
+
+// const obj = {
+// 	a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Dima';
+
+// {
+// 	const num = 10;
+// }
+
+// console.log(num);
+
+const vehicleBodyWidth = 5000;
+const vehicleBodyLenght = 4000;
+
+console.log('Ширинка кузова автомобиля: ' + vehicleBodyWidth + ', длина: ' + vehicleBodyLenght);
+
+// ['vrere', 'cscscs', 'aaa'].map(a =>)
+
+// snake_case
+// UPPER_SNAKE_CASE
+
+const COLOR_RED = '#F00';
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const person = 'Alex';
+
+const bool = true;
+
+// console.log(ahaahah);
+
+let und;
+console.log(und)
+
+const obj = {
+	name: 'Dima',
+	age: 23,
+	isMarried: false,
+};
+
+console.log(obj.age);
+console.log(obj['name']);
+
+
+let arr = ['plum.png', 'orange.png', 'apple.bpm'];
+console.log(arr[1]);
+
+// let name = "Ilya";
+
+// alert( `hello ${1}` );
+
+// alert( `hello ${"name"}` );
+
+// alert( `hello ${name}` );

ed8a1de903307447289a11c045171ba36f3fe63d
Dima <exzmple@gmail.com>
2023-02-17 14:07:42 +0200

    first copmmit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 // a = 15;
 // console.log(a);
@@ -25,44 +25,44 @@
 
 // console.log(num);
 
-const vehicleBodyWidth = 5000;
-const vehicleBodyLenght = 4000;
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLenght = 4000;
 
-console.log('Ширинка кузова автомобиля: ' + vehicleBodyWidth + ', длина: ' + vehicleBodyLenght);
+// console.log('Ширинка кузова автомобиля: ' + vehicleBodyWidth + ', длина: ' + vehicleBodyLenght);
 
 // ['vrere', 'cscscs', 'aaa'].map(a =>)
 
 // snake_case
 // UPPER_SNAKE_CASE
 
-const COLOR_RED = '#F00';
+// const COLOR_RED = '#F00';
 
-let number = 4.6;
+// let number = 4.6;
 
-console.log(-4/0);
-console.log('string' * 9);
+// console.log(-4/0);
+// console.log('string' * 9);
 
-const person = 'Alex';
+// const person = 'Alex';
 
-const bool = true;
+// const bool = true;
 
-// console.log(ahaahah);
+// // console.log(ahaahah);
 
-let und;
-console.log(und)
+// let und;
+// console.log(und)
 
-const obj = {
-	name: 'Dima',
-	age: 23,
-	isMarried: false,
-};
+// const obj = {
+// 	name: 'Dima',
+// 	age: 23,
+// 	isMarried: false,
+// };
 
-console.log(obj.age);
-console.log(obj['name']);
+// console.log(obj.age);
+// console.log(obj['name']);
 
 
-let arr = ['plum.png', 'orange.png', 'apple.bpm'];
-console.log(arr[1]);
+// let arr = ['plum.png', 'orange.png', 'apple.bpm'];
+// console.log(arr[1]);
 
 // let name = "Ilya";
 
@@ -71,3 +71,92 @@ console.log(arr[1]);
 // alert( `hello ${"name"}` );
 
 // alert( `hello ${name}` );
+
+// const arr = [1, 2, 3];
+// const arrObj = {
+// 	0: 1,
+// 	1: 2,
+// 	2: 3,
+// };
+
+// arrObj.b = '1234';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// const obj1 = {
+// 	Anna: 500,
+// 	Alice: 800,
+// };
+
+// function getArray(arr, parent =  document.body) {
+      
+// 	let elemList = document.createElement('ul');
+// 	document.querySelector('body').prepend(elemList);
+   
+// 	// for ( let i = 0; i < arr.length; i++){
+		 
+		  
+		  
+// 		  arr.forEach(function (el, index, arr) {
+// 			let elemLi = document.createElement('li');
+// 			elemList.prepend(elemLi);	
+// 			elemLi.textContent = el;
+// 		  });
+
+		  
+// 	}
+   
+
+// getArray(['hello', 'world', 'Kiev', 'Kharkiv', 'Odessa', 'Kiev', 'Kharkiv',]);
+// alert('hello');
+
+// const result = confirm('Are you here');
+
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '');
+// console.log(answer + 5);
+
+// const anwsers = [];
+
+// anwsers[0] = prompt('как ваше имя');
+// anwsers[1] = prompt('как ваше фамилия');
+// anwsers[2] = prompt('сколько вам лет');
+
+// console.log(typeof(anwsers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'dima';
+
+// alert(`hey, ${user}`);
+
+console.log('arr' + ' - object');
+
+console.log(4 + +'4');
+
+let incr = 10,
+	decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== 8)
+
+// && // оператор и - когда 2 и больше значений есть правдой
+// || // оператор или - если есть одна правда
+
+const isCheked = true,
+	  isClose = false;
+	
+console.log(isCheked || !isClose);
